f820f6d3324b4dadb736f3835faa19da3133262a
Anna Kovalik <annhuddy13@gmail.com>
2023-04-14 20:15:31 +0300

    Feat: add minor style changes

--- src/CustomSelect.tsx
+++ src/CustomSelect.tsx
@@ -17,6 +17,7 @@ const CustomSelect: React.FC<Props> = ({ renderData }) => {
 
   const selectActiveItem = (selectedItem: DataObject) => {
     setActiveItem(selectedItem);
+    setShowList(false);
   };
 
   return (
@@ -24,6 +25,23 @@ const CustomSelect: React.FC<Props> = ({ renderData }) => {
       <div className="custom-select">
         <button className="item active-item" onClick={() => setShowList(!showList)}>
           {activeItem.title}
+          <svg
+            className={`icon ${showList ? "open" : ""}`}
+            width="9"
+            height="16"
+            viewBox="0 0 9 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M7 10C7.82405 10 8.29443 10.9408 7.8 11.6L5.3 14.9333C4.9 15.4667 4.1 15.4667 3.7 14.9333L1.2 11.6C0.705573 10.9408 1.17595 10 2 10L7 10Z"
+              fill="#6F767E"
+            />
+            <path
+              d="M7 6C7.82405 6 8.29443 5.05924 7.8 4.4L5.3 1.06667C4.9 0.533334 4.1 0.533334 3.7 1.06667L1.2 4.4C0.705573 5.05924 1.17595 6 2 6L7 6Z"
+              fill="#6F767E"
+            />
+          </svg>
         </button>
         <div className={`list ${showList ? "visible" : ""}`}>
           {renderData.map((item) => {
@@ -39,10 +57,16 @@ const CustomSelect: React.FC<Props> = ({ renderData }) => {
           })}
         </div>
       </div>
-      <select name="test-select" id="select" className="native-select" onChange={changeHandler}>
+      <select
+        name="test-select"
+        id="select"
+        className="native-select"
+        onChange={changeHandler}
+        value={activeItem.title}
+      >
         {renderData.map((item) => {
           return (
-            <option key={item.id} value={item.title} selected={item.title === activeItem.title}>
+            <option key={item.id} value={item.title}>
               {item.title}
             </option>
           );
